dfdeebadc5749e853687b5a40d9cc4cd9f53bb56
smauhourat <santiagomauhourat@hotmail.com>
2020-10-16 13:13:31 -0300

    initial

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const StartGameScreen = (props) => {
+    return (
+    );
+};
+
+const styles = StyleSheet.create({
+  });
+  
+export default StartGameScreen;
